5380b1a597a2e54e57e2c539a18bb2de29de10a4
Dreshi <73090979+Dreshi@users.noreply.github.com>
2023-01-14 12:07:18 +1300

    add new campground functionality

--- app.js
+++ app.js
@@ -16,19 +16,35 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+// shows all campgrounds of the database
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds})
 })
 
+// add a new campground (needs to be before the detail page - won't find new otherwise)
+app.get('/campgrounds/new', async (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+// show detail of the campground
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id) 
     res.render('campgrounds/show', {campground});
 })
+
 
 // Testing whether a new document is created
 // app.get('/makecampground', async (req, res) => {
